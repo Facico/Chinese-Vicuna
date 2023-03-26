126824b0763d4448f4c083c8735f9d025c6ffc08
LZY-the-boys <2403123085@qq.com>
2023-03-26 21:06:00 +0800

    fix bug

--- generate_chatglm.py
+++ generate_chatglm.py
@@ -22,10 +22,9 @@ extra requirements:
     pip install icetk
 """
 
-
-# reload the model: no int8
-version = 'finetune_0.pt'
-model_dir = '/model/chatglm-6b'
+# reload the model: no int8, so 14GB is needed
+version = 'no.pt' # finetune_0.pt
+model_dir = '/home/liang/lzy_tmp/models/chatglm-6b'
 tokenizer = AutoTokenizer.from_pretrained(model_dir, trust_remote_code=True)
 model = AutoModel.from_pretrained(model_dir, trust_remote_code=True)
 config = LoraConfig(
@@ -60,27 +59,31 @@ class QKV_layer(torch.nn.Module):
         v = self.linear_v(x)
         return torch.concat([q,k,v], dim = -1)
 
-# convert it again
-for key, module in model.named_modules():
-    if key.endswith('attention'):
-        try:
-            qkv_layer = QKV_layer(module.query_key_value.in_features, module.query_key_value.out_features) 
-            qkv_layer.update(module.query_key_value)
-            module.query_key_value = qkv_layer
-        except:
-            pass
-        module.query_key_value = peft.tuners.lora.LoraModel(config, module.query_key_value)
+if version != 'no.pt':
+    # convert it again
+    for key, module in model.named_modules():
+        if key.endswith('attention'):
+            try:
+                qkv_layer = QKV_layer(module.query_key_value.in_features, module.query_key_value.out_features) 
+                qkv_layer.update(module.query_key_value)
+                module.query_key_value = qkv_layer
+            except:
+                pass
+            module.query_key_value = peft.tuners.lora.LoraModel(config, module.query_key_value)
 
-# load the LoRA checkpoint
-model.load_state_dict(torch.load(f'/{model_dir}/{version}'), strict=False)
+    # load the LoRA checkpoint
+    model.load_state_dict(torch.load(f'/{model_dir}/{version}'), strict=False)
+
 model.half().cuda().eval()
 
 # Let's chat!
-with open('outs/chatglm-6b/test_{version}.txt') as f:
-    for text in open('datasets/test.jsonl'):
-        text = json.loads(text).strip()
-        print('Q:', test)
-        print('Q:', test, file=f)
-        response, history = model.chat(tokenizer, text, history=[])
+os.makedirs('outs/chatglm-6b/', exist_ok=True)
+with open(f'outs/chatglm-6b/test_{version}.txt','w') as f:
+    for text in open('sample/test.jsonl'):
+        text = json.loads(text)
+        inputs = text['instruction']
+        print('Q:', inputs)
+        print('Q:', inputs, file=f)
+        response, history = model.chat(tokenizer, inputs, history=[])
         print('A:', response)
-        print('A:', test, file=f)
+        print('A:', response, '\n',file=f)
