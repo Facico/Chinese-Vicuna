a8886ff7f5e945fa53b0303c8306d20bc3bd8b6d
Facico <845161327@qq.com>
2023-04-02 23:46:58 +0800

    fix the "==" in bash

--- chat.sh
+++ chat.sh
@@ -1,19 +1,19 @@
-BASE_MODEL="/model/13B_hf"
-LORA_PATH="checkpoint-6000"
-USE_LOCAL=0 # 0: use local model, 1: use huggingface model
+BASE_MODEL="decapoda-research/llama-7b-hf" #"/model/13B_hf"
+LORA_PATH="Chinese-Vicuna/Chinese-Vicuna-lora-7b-belle-and-guanaco" #"checkpoint-6000"
+USE_LOCAL=0 # 1: use local model, 0: use huggingface model
 DEBUG=0
-if [ USE_LOCAL == 1 ]
+if [ USE_LOCAL==1 ]
 then
 cp ./config-sample/adapter_config.json $LORA_PATH
 fi
 
-if [ DEBUG == 1 ]
+if [ DEBUG==1 ]
 then
 jurigged -v chat.py --model_path $BASE_MODEL --lora_path $LORA_PATH --use_local $USE_LOCAL\
  --use_typewriter 1 \
  --share_link 0 
 else
-python chat.py --model_path $BASE_MODEL --lora_path $LORA_PATH --use_local $USE_LOCAL\
+CUDA_VISIBLE_DEVIECES=0 python chat.py --model_path $BASE_MODEL --lora_path $LORA_PATH --use_local $USE_LOCAL\
     --use_typewriter 1 \
     --share_link 0 
 fi
